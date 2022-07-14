6d4cc0cb22552fa34e5d8c3c34a690d067f58cfd
Nikolai <nikolai71@msn.com>
2022-07-14 22:03:23 +0300

    create new file1

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,17 +1,15 @@
 ﻿Console.WriteLine("Введите три числа : ");
 
-        int a = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
-        int b = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
-        int c = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 
-        int max = a;
+int max = a;
         
-        if (max < b) max = b;
+if (max < b) max = b;
 
-        if (max < c) max = c;
+if (max < c) max = c;
 
-        
-        Console.WriteLine("{0} наибольшее число", max);
-        
+Console.WriteLine("{0} наибольшее число", max);
