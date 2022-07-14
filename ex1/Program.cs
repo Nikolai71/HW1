c8f601f80e321bf66fc4f39cfad1be0b565802c4
Nikolai <nikolai71@msn.com>
2022-07-14 21:38:47 +0300

    create new file1

---
+++ ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите два числа : ");
+ 
+        int a = Convert.ToInt32(Console.ReadLine());
+ 
+        int b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+  
+        else
+  
+            Console.WriteLine("{0} наибольшее число ", b);
+        
+        if (a < b)
+
+            Console.WriteLine("{0} наименьшее", a);
+
+        else
+
+            Console.WriteLine("{0} наименьшее число", b);
